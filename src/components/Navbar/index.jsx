2da4f5833661c2aff2a06ba40153e5b5710e05b0
Gabriela Queiroz <gabrielapg.queiroz@gmail.com>
2023-09-13 10:29:27 -0300

    iniciando projeto e adicionando navbar e footer

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,24 @@
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="deep-orange darken-4">
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo">
+            MENU PROFIT
+          </a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li>
+              <a href="sass.html">Sass</a>
+            </li>
+            <li>
+              <a href="badges.html">Components</a>
+            </li>
+            <li>
+              <a href="collapsible.html">JavaScript</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
